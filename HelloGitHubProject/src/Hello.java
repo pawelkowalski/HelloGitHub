61621b67b091bf3b5e6b7cf058ee41b561218dbe
Pawel Kowalski <kowal44@gmail.com>
2012-01-16 22:09:37 +0100

    New project created

---
+++ HelloGitHubProject/src/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Pawel Kowalski <BR>
+ *         mailto: kowal44@gmail.com
+ * 
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
